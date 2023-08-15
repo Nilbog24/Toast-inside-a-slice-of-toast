c32b8e952abe4af7932ff9a6d084826fe965f45a
Nilbog24 <brianngan2406@gmail.com>
2023-08-15 15:14:44 +1200

    part 2/3 of character manager creation.

--- TiaSoTiaSoT/Assets/Scripts/Visual Novel Script/Character.cs
+++ TiaSoTiaSoT/Assets/Scripts/Visual Novel Script/Character.cs
@@ -1,12 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;   
 
 
 [System.Serializable]
-public class Character : MonoBehaviour
+public class Character 
 {
     public string name;
+    /// <summary>
+    /// The root is the container for all of the images related ot the character in the scene.
+    /// </summary>
+    [HideInInspector] public RectTransform root;
 
+     DialogueSystem dialogue;
+
+    public void Say(string speech)
+    {
+        dialogue.Say(speech, characterName);
+    }
+
+    /// <summary>
+    /// Create a new character.
+    /// </summary>
+    /// <param name="_name"></param>
+    public Character(string _name)
+    {
+        CharacterManager cm = CharacterManager.instance;
+        // Locate the character prefab.
+        GameObject prefab = Resources.Load("Characters/Character["+_name+"]") as GameObject;
+        // Spawn an instance of the prefab directily on the character layer.
+        GameObject ob = GameObject.Instantiate(prefab, cm.CharacterLayer);
+
+        root = ob.GetComponent<RectTransform> ();
+        name = _name;
+
+        renderers.renderer = ob.GetComponentInChildren<RawImage> ();
+
+        dialogue = DialogueSystem.instance;
+
+    }
+
+    [System.Serializable]
+    public class Renderers
+    {
+        /// <summary>
+        /// used as the only image for a single layer character.
+        /// </summary>
+        public RawImage renderer;
+        //Sprites use images
+        /// <summary>
+        /// The body renderer for a multi layer character.
+        /// </summary>
+        public Image bodyRenderer;
+        /// <summary>
+        /// The expression renderer for a multi layer character.
+        /// </summary>
+        public Image expressionRenderer;
+    }
+
+    public Renderers renderers = new Renderers();
 }
--- TiaSoTiaSoT/Assets/Scripts/Visual Novel Script/CharacterManager.cs
+++ TiaSoTiaSoT/Assets/Scripts/Visual Novel Script/CharacterManager.cs
@@ -20,9 +20,47 @@ public class CharacterManager : MonoBehaviour
     /// </summary>
     public List<Character> characters = new List<Character>();
     
-    public Dictionary
+    /// <summary>
+    /// Easy lookup for  our characters.
+    /// </summary>
+    public Dictionary<string, int> characterDictionary = new Dictionary<string, int>();
     void Awake()
     {
         instance = this;
+    }
+
+    /// <summary>
+    /// Tries to get a charavter by the name  provided from the character list.
+    /// </summary>
+    /// <param name="characterName"></param>
+    public Character GetCharacter(string characterName, bool createCharacterIfDoesNotExist = true)
+    {
+        // Search our dictionary to find the character quickly if  it is already in our scene.
+        int index  = -1;
+        if (characterDictionary.TryGetValue(characterName, out index))
+        {
+            return characters [index];
+        }
+        else if (createCharacterIfDoesNotExist)
+        {
+            return CreateCharacter(characterName);
+        }
+
+        return null;
+    }
+
+    /// <summary>
+    /// Creates the character
+    /// </summary>
+    /// <param name="characterName"></param>
+    /// <returns></returns>
+    public Character CreateCharacter(string characterName)
+    {
+        Character newCharacter = new Character (characterName);
+
+        characterDictionary.Add(characterName, characters.Count);
+        characters.Add(newCharacter);
+
+        return newCharacter;
     }
 }
